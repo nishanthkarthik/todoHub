3dd5fe778650646ec93bb174c2dff76010360977
Karthik Nishanth <nishanthkarthik@live.com>
2015-12-27 16:23:45 +0530

    Reset server.js

--- server.js
+++ server.js
@@ -1,30 +1,14 @@
-// server.js
 
-// set up ========================
-var express  = require('express');
-var app      = express();                               // create our app w/ express
-var mongoose = require('mongoose');                     // mongoose for mongodb
-var morgan = require('morgan');             // log requests to the console (express4)
-var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var express = require('express');
+var app = express();
 
-// configuration =================
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
+var server = app.listen(8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride());
-
-// listen (start app with node server.js) ======================================
-app.listen(8080);
-console.log("App listening on port 8080");
-
-app.get('/index', function (req, res) 
-{
-    res.send('Hello World!');
-    console.log('Main');
+  console.log('Example app listening at http://%s:%s', host, port);
 });
